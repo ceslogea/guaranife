c3ae00726c21c142a22f6ea2c3255b8b6950f51e
Celso Aparecido Géa Bernar Junior <celso.bernar@programmers.com.br>
2018-06-06 14:02:19 -0300

    Adicionado layout estático

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
+  // { path: '/', component: AppComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      { enableTracing: false } // <-- debugging purposes only
     )
   ],
   providers: [],
--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,6 +1,6 @@
 
 jQuery(document).ready(function() {
-	
+	console.log("teste")
 	/*
 	    Wow
 	*/
